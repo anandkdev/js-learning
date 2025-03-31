7a87ff9bfa0aed31654e3a3a288c521c4e9e548a
Anand-121998 <ananda.kumar@alferix.com>
2025-03-31 22:45:02 +0530

    Advance array and loop with additional feature like break, continue,and return

--- js file/11-todo.js
+++ js file/11-todo.js
@@ -15,9 +15,14 @@
       addtodo = '';
       for(let i=0; i < todoList.length; i++){
       const value = todoList[i];
-      const html = `<p>${value}
+      console.log(value);
+      // const name = todoList.name;
+      // const dueDate = todoList.dueDate;
+      const {name, dueDate} = value;
+      const html = `<p>${name} ${dueDate}
       <button onclick = "deleteIndex(${i})">Delete 1</button>
       <button onclick = "todoList.splice(${i}, 1); renderToDoList()">Delete 2</button></p>`;
+      console.log(html);
       addtodo += html;
       
     }
@@ -34,9 +39,12 @@
  
     function addToDo(){
       let inputA = document.querySelector('.js-input');
+      let dAte = document.querySelector('.js-date');
+      let dueDate = dAte.value;
       const name = inputA.value;
+      console.log(dueDate);
       // console.log(name);
-      todoList.push(name);
+      todoList.push({name, dueDate});
       // console.log(todolist);
       inputA.value = ""; 
 
